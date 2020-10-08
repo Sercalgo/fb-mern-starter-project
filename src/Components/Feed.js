5d53c4c0e94e9c1b2d09d8a7dc00f058a3a98032
Sercalgo <Sercalgo@github.com>
2020-10-08 11:45:59 +0200

    Front end with feed finished and realtime

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,41 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import './Feed.css'
-import MessageSender from './MessageSender'
-import Post from './Post'
-import StoryReel from './StoryReel'
-import axios from '../axios'
-import Pusher from 'pusher-js'
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import MessageSender from "./MessageSender";
+import Post from "./Post";
+import StoryReel from "./StoryReel";
+import axios from "../axios";
+import Pusher from "pusher-js";
+import db from "../firebase";
 
-import db from '../firebase'
+var pusher = new Pusher("47bf8abc5a7a9491829b", {
+  cluster: "eu",
+});
 
 const Feed = () => {
-    const [profilePic, setProfilePic] = useState('')
-    const [postsData, setPostsData] = useState([])
+  const [profilePic, setProfilePic] = useState("");
+  const [postsData, setPostsData] = useState([]);
 
-    useEffect(() => {
-        db.collection('posts').onSnapshot(snapshot => (
-            setPostsData(snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() })))
-        ))
-    }, [])
+  const syncFeed = () => {
+    axios.get("/retrieve/posts").then((res) => {
+      console.log(res.data);
+      setPostsData(res.data);
+    });
+  };
 
-    return (
-        <div className='feed' >
-            <StoryReel />
-            <MessageSender />
+  useEffect(() => {
+    const channel = pusher.subscribe("posts");
+    channel.bind("inserted", function (data) {
+      syncFeed();
+    });
+  }, []);
 
-            {
-                postsData.map(entry => (
-                    <Post
-                        profilePic={entry.avatar}
-                        message={entry.text}
-                        timestamp={entry.timestamp}
-                        imgName={entry.imgName}
-                        username={entry.user}
-                    />
-                ))
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    syncFeed();
+  }, []);
 
-export default Feed
+  return (
+    <div className="feed">
+      <StoryReel />
+      <MessageSender />
+
+      {postsData.map((entry) => (
+        <Post
+          profilePic={entry.avatar}
+          message={entry.text}
+          timestamp={entry.timestamp}
+          imgName={entry.imgName}
+          username={entry.user}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Feed;
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,16 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    // this is where your firebase config goes
-};
+
+    apiKey: "AIzaSyDQ3FlJpXBL3f_OKGGUxOXxup3KS8-2Qto",
+    authDomain: "fbclonemern.firebaseapp.com",
+    databaseURL: "https://fbclonemern.firebaseio.com",
+    projectId: "fbclonemern",
+    storageBucket: "fbclonemern.appspot.com",
+    messagingSenderId: "7822815601",
+    appId: "1:7822815601:web:90f0156c811a332674c6d0",
+    measurementId: "G-8GVMQHZXNM"
+}
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
@@ -10,5 +18,8 @@ const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
 const db = firebase.firestore()
 
-export { auth, provider }
+export {
+    auth,
+    provider
+}
 export default db
